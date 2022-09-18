5ed6aef8a5f137b81587542778ea282b20f82fa2
Basil <Basil.muteab@gmail.com>
2022-09-18 19:11:30 -0400

    breeking up the string then sorting then reversing.

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,3 +1,12 @@
 
-const reversorder = str => str;
+const reversorder = str => {
+    let arr=str.split("");
+    console.log("after split " +arr)
+    arr= arr.sort();
+    console.log("sort: " +arr)
+    arr= arr.reverse();
+    console.log("reverse: " +arr)
+    return arr;
+};
 
+console.log(reversorder("exioi"))
