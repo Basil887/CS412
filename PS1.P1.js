b2cfb1d95b06339b6b1e2a544f710edcf4e229d0
Basil <Basil.muteab@gmail.com>
2022-09-18 19:14:02 -0400

    breeking up the string then sorting then reversing then joining omitting test.

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,13 +1,13 @@
 
 const reversorder = str => {
     let arr=str.split("");
-    console.log("after split " +arr);
+    //console.log("after split " +arr);
     arr= arr.sort();
-    console.log("sort: " +arr);
+    //console.log("sort: " +arr);
     arr= arr.reverse();
-    console.log("reverse: " +arr);
+    //console.log("reverse: " +arr);
     arr=arr.join("");
     return arr;
 };
 
-console.log(reversorder("exioi"))
+console.log(reversorder("supercalifragilisticexpialidocious"))
