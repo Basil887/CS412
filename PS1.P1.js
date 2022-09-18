097edcd31d49549de04222d0a0150051d2cc581a
Basil <Basil.muteab@gmail.com>
2022-09-18 19:13:00 -0400

    breeking up the string then sorting then reversing then joining .

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,11 +1,12 @@
 
 const reversorder = str => {
     let arr=str.split("");
-    console.log("after split " +arr)
+    console.log("after split " +arr);
     arr= arr.sort();
-    console.log("sort: " +arr)
+    console.log("sort: " +arr);
     arr= arr.reverse();
-    console.log("reverse: " +arr)
+    console.log("reverse: " +arr);
+    arr=arr.join("");
     return arr;
 };
 
