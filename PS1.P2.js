71766e281acc745a661a5213f960c5ebf1e2de35
Basil <Basil.muteab@gmail.com>
2022-09-18 19:38:24 -0400

    helper function

--- PS1.P2.js
+++ PS1.P2.js
@@ -21,3 +21,10 @@ const operator = expr => {
     }
     return expr;
 };
+
+const vals = expr =>{
+    let [val1, op, val2]= expr.split("");
+    let left=val1;
+    let right=val2
+    return left,right;
+}
