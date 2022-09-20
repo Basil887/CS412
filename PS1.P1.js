83bd1a3f7d35f4259c4dae0849463ebf927d473d
Basil <Basil.muteab@gmail.com>
2022-09-20 17:07:01 -0400

    finished diffrent way

--- PS1.P1.js
+++ PS1.P1.js
@@ -1,12 +1,10 @@
 
 const reversorder = str => {
-    let arr=str.split("");
+    let arr=str.split("").sort().join("");
     //console.log("after split " +arr);
-    arr= arr.sort();
     //console.log("sort: " +arr);
-    arr= arr.reverse();
     //console.log("reverse: " +arr);
-    arr=arr.join("");
+
     return arr;
 };
 
